74c1a82dd2431bcffec18373a1e22563266191cc
Namdak Tonpa <maxim@synrc.com>
2019-11-02 15:31:01 +0200

    enable iex -S mix

---
+++ config/config.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+config :kvs,
+  dba: :kvs_mnesia,
+  dba_st: :kvs_stream,
+  schema: [:kvs, :kvs_stream, :bpe_metainfo]
+
+config :rest,
+  logger_level: :debug,
+  logger: [{:handler, :synrc, :logger_std_h,
+            %{level: :debug,
+              id: :synrc,
+              module: :logger_std_h,
+              config: %{type: :file, file: 'bpe.log'},
+              formatter: {:logger_formatter,
+                          %{template: [:time,' ',:pid,' ',:msg,'\n'],
+                            single_line: true,}}}}]
