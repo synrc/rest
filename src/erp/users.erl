25969d7018469f76d93881ff192d99786c4b24bc
Fabian N.C. van 't Hooft <fnchooft@gmail.com>
2021-04-07 16:37:03 -0300

    Fixes get(Id) for users plus get-all endpoint
    The users.erl file need a minor tweak to actually
    get the data from the ets-table.
    
    Also the cowboy-handler needed a clause to return
    all users if it was requested.

--- src/erp/users.erl
+++ src/erp/users.erl
@@ -9,7 +9,7 @@ init()               -> ets:new(users, [public, named_table, {keypos, #user.id}]
 populate(Users)      -> ets:insert(users, Users).
 exists(Id)           -> X = ets:member(users, binary_to_list(Id)), io:format("Member: ~p~n",[X]), X.
 get()                -> ets:tab2list(users).
-get(Id)              -> #user{id=Id}.
+get(Id)              -> [U] = ets:lookup(users, binary_to_list(Id)), io:format("User: ~p~n",[U]), U.
 delete(Id)           -> ets:delete(users, binary_to_list(Id)).
 post(#user{} = User) -> ets:insert(users, User);
 post(Data)           -> post(from_json(Data, #user{})).
